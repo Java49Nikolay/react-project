9870ba0032eac255a03b22ab9d5e594c1e972533
Nik.choporev <nik.choporev@gmail.com>
2023-01-19 12:07:27 +0200

    CW #33 Input, Alert components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Timer } from './components/Timer';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const flexColumn: React.CSSProperties = { display: "flex", flexDirection: "column" }
+  const flexRow: React.CSSProperties = { display: "flex", flexDirection: "row" , 
+  justifyContent: "space-around", width: "50vw", marginTop: "4vh"}
+  return <div style={flexColumn}>
+    <div style={flexRow}>
+      <Timer cityOrCountry="London"></Timer>
+      <Timer cityOrCountry="Paris"></Timer>
     </div>
-  );
+    <div style={flexRow}>
+      <Timer cityOrCountry="India "></Timer>
+      <Timer cityOrCountry={"Cuba"}></Timer>
+    </div>
+
+  </div>
+
 }
 
 export default App;
